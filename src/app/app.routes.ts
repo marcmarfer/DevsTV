7822b5185a7192b1db015a55b8a73fc1c27bee58
Marc Marqués Fernández <marcmarfer@campus.monlau.com>
2024-05-03 17:40:51 +0200

    added routing and displaying header, main and footer views

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './main/home/home.component';
+import { VideosComponent } from './main/videos/videos.component';
+import { AdminComponent } from './main/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'videos', component: VideosComponent },
+    { path: 'admin', component: AdminComponent },
+    { path: '**', redirectTo: '' }
+];
