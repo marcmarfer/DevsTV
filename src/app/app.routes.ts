6bc6fdb735720ff6a13a612840bd02704c1396ae
Marc Marqués Fernández <marcmarfer@campus.monlau.com>
2024-05-07 19:24:25 +0200

    home page deleted and added bookmarks, hover animations on videos and responsive dimensions for each miniature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/main/home/home.component';
+import { BookmarksComponent } from './components/main/bookmarks/bookmarks.component';
 import { VideosComponent } from './components/main/videos/videos.component';
 import { AdminComponent } from './components/main/admin/admin.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'home', component: HomeComponent },
+    { path: '', component: VideosComponent },
     { path: 'videos', component: VideosComponent },
+    { path: 'bookmarks', component: BookmarksComponent },
     { path: 'admin', component: AdminComponent },
     { path: '**', redirectTo: '' }
 ];
--- src/app/components/main/bookmarks/bookmarks.component.spec.ts
+++ src/app/components/main/bookmarks/bookmarks.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { BookmarksComponent } from './bookmarks.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('BookmarksComponent', () => {
+  let component: BookmarksComponent;
+  let fixture: ComponentFixture<BookmarksComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [BookmarksComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(BookmarksComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
