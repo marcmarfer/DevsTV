38b395f14b1583b13912f10fd5fedc1c1c639ab2
Marc Marqués Fernández <marcmarfer@campus.monlau.com>
2024-05-10 18:22:57 +0200

    added password field on users table

--- api/database.sql
+++ api/database.sql
@@ -23,13 +23,13 @@ CREATE TABLE bookmarked_videos (
     FOREIGN KEY (id_video) REFERENCES videos(id_video)
 );
 
-INSERT INTO users (name, email) VALUES
+INSERT INTO users (name, email, password) VALUES
 ('User1', 'user1@example.com', 'password1'),
 ('User2', 'user2@example.com', 'password2');
 
-INSERT INTO videos (title, description, youtube_url, thumbnail_url, category) VALUES
-('Video1', 'Description of video 1', 'https://www.youtube.com/watch?v=u_OBuuR1f-Q&list=LL', 'https://cadenaser.com/resizer/Y2SyvGDiaElqtiN1-npSl7yH8ng=/1200x900/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/prisaradio/EULQ4EZPKRIE7OUVZHW3H2FYD4.jpg', 'Web Development'),
-('Video2', 'Description of video 2', 'https://www.youtube.com/watch?v=u_OBuuR1f-Q&list=LL', 'https://cadenaser.com/resizer/Y2SyvGDiaElqtiN1-npSl7yH8ng=/1200x900/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/prisaradio/EULQ4EZPKRIE7OUVZHW3H2FYD4.jpg', 'Cloud Computing');
+INSERT INTO videos (title, youtube_url, thumbnail_url, category) VALUES
+('Video1', 'https://www.youtube.com/watch?v=u_OBuuR1f-Q&list=LL', 'https://cadenaser.com/resizer/Y2SyvGDiaElqtiN1-npSl7yH8ng=/1200x900/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/prisaradio/EULQ4EZPKRIE7OUVZHW3H2FYD4.jpg', 'Web Development'),
+('Video2', 'https://www.youtube.com/watch?v=u_OBuuR1f-Q&list=LL', 'https://cadenaser.com/resizer/Y2SyvGDiaElqtiN1-npSl7yH8ng=/1200x900/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/prisaradio/EULQ4EZPKRIE7OUVZHW3H2FYD4.jpg', 'Cloud Computing');
 
 INSERT INTO bookmarked_videos (id_user, id_video, rating) VALUES
 (1, 1, 5),
