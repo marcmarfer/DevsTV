8e541e68f027b4b1d663eaa1d0debec71efd0e56
Marc Marqués Fernández <marcmarfer@campus.monlau.com>
2024-05-14 20:15:05 +0200

    added login and register components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { BookmarksComponent } from './components/main/bookmarks/bookmarks.component';
 import { VideosComponent } from './components/main/videos/videos.component';
 import { AdminComponent } from './components/main/admin/admin.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
     { path: '', component: VideosComponent },
     { path: 'videos', component: VideosComponent },
     { path: 'bookmarks', component: BookmarksComponent },
     { path: 'admin', component: AdminComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     { path: '**', redirectTo: '' }
 ];
