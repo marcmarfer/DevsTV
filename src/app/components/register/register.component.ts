023082363a5b2339016dda19108173476ef4c5cb
unknown <marcmf2004@gmail.com>
2024-05-15 22:42:07 +0200

    advanced register page

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm: FormGroup = new FormGroup({});
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+      confirmPassword: new FormControl('', Validators.required)
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Form submitted');
+      this.registerForm.reset();
+    }
+  }
 }
