d56fcd10e0182f75a01e814a839027e27f320fbb
Marc Marqués Fernández <marcmf2004@gmail.com>
2024-05-15 11:34:32 +0200

    added a function to hide header and footer in the registration and login url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'DevsTV';
+
+  constructor(private router: Router) {}
+
+  isLoginPageOrRegisterPage(): boolean {
+    return this.router.url === '/login' || this.router.url === '/register';
+  }
 }
